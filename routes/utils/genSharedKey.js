ff081dced9513d36697d4137186b9632c5800649
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2020-11-02 09:29:45 +0900

    Refactor genSharedKey return value

--- routes/utils/genSharedKey.js
+++ routes/utils/genSharedKey.js
@@ -3,7 +3,7 @@ const base32 = require('base32');
 function genSharedKey() {
   const random12 = `${Math.random() * Math.pow(10, 20)}`.substring(0, 12);
 
-  return (K = base32.encode(random12));
+  return base32.encode(random12);
 }
 
 module.exports.genSharedKey = genSharedKey;
